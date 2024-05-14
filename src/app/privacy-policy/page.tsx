1e1b789012dcea49acfd08cafd4f7d1312a211bd
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2024-05-14 23:32:15 +0530

    added essential page structures

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PrivacyPolicy() {
+  return <main>PrivacyPolicy</main>;
+}
+
+export default PrivacyPolicy;
---
+++ src/app/terms-conditions/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TermsAndConditionsPage() {
+  return <main>TermsAndConditionsPage</main>;
+}
+
+export default TermsAndConditionsPage;
