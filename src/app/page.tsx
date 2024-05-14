ca611fc7bd778bd112340ddf59de3367bf78b125
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2024-05-15 00:45:12 +0530

    added header and main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,28 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Find events around you</h1>
-      <p>Browse more than 10,000 events around you</p>
-      <form>
-        <input placeholder="Search events in your city..." spellCheck={false} />
+    <main className="flex flex-col items-center pt-36 px-3">
+      <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
+        Find events around you
+      </h1>
+      <p className="mb-12 mt-3 lg:mt-7 text-2xl lg:text-4xl opacity-75">
+        Browse more than{" "}
+        <span className="font-bold italic underline text-[#a4f839]">
+          10,000
+        </span>{" "}
+        events around you
+      </p>
+      <form className="w-full sm:w-[500px]">
+        <input
+          className="w-full sm:w-[500px] rounded-lg bg-white/[7%] outline-none px-6 h-16 ring-[#a4f839]/50 focus:ring-1 transition focus:bg-white/10"
+          placeholder="Search events in your city..."
+          spellCheck={false}
+        />
       </form>
 
-      <section>
+      <section className="mt-4 flex items-center gap-x-4 text-sm text-white/50">
         <p>Popular:</p>
-        <div>
+        <div className="flex gap-x-2">
           <Link href="/events/austin">Austin</Link>
           <Link href="/events/seattle">Seattle</Link>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,33 @@ import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
 
+const routes = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/events/all",
+    label: "All Events",
+  },
+];
+
 function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <Link href="/">Home</Link>
-      <Link href="events/all">All Events</Link>
+      <nav>
+        <ul className="flex gap-x-6 text-sm">
+          {routes.map((route) => (
+            <li
+              key={route.href}
+              className="text-white/50 hover:text-white transition duration-250"
+            >
+              <Link href={route.href}>{route.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
