feeee02ad99f3758afedc75719650fb15ef6223a
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2024-05-15 01:00:27 +0530

    added footer

---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col max-w-7xl mx-auto bg-white/[2%] min-h-screen">
+      {children}
+    </div>
+  );
+}
+
+export default Container;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
 import React from "react";
 
+const routes = [
+  { path: "/privacy-policy", name: "Privacy Policy" },
+  { path: "/terms-conditions", name: "Terms & Conditions" },
+];
+
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="mt-auto flex items-center justify-between h-16 border-t border-white/10 px-3 sm:px-9 text-xs text-white/25 ">
+      <small className="text-xs">&copy; 2050 AP. All rights reserved.</small>
+      <ul className="flex gap-x-3 sm:gap-x-6">
+        {routes.map((route) => (
+          <li key={route.path}>
+            <Link href={route.path}>{route.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer;
