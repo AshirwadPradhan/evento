3c5cd8f913e6e113a6b9d67d7493cac3990bfaed
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2024-05-15 17:23:59 +0530

    added headings for all event pages

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,10 +1,17 @@
 import Heading from "@/components/Heading";
 import React from "react";
 
-function EventsPage() {
+function EventsPage({ params }: { params: { city: string } }) {
+  const city = params.city;
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
-      <Heading>Events in Austin</Heading>
+      {params.city === "all" ? (
+        <Heading>All Events</Heading>
+      ) : (
+        <Heading>
+          Events in {city.charAt(0).toUpperCase() + city.slice(1)}
+        </Heading>
+      )}
     </main>
   );
 }
