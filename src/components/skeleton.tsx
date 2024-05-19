e7b63cb1da41c28ed09fdf3ec566b28d4a940f60
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2024-05-19 13:56:32 +0530

    added skeleton for loading page

---
+++ src/components/skeleton.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+
+function Skeleton({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        "animate-pulse h-4 w-[550px] rounded-md bg-white/5",
+        className
+      )}
+    ></div>
+  );
+}
+
+export default Skeleton;
